8008cdfcdcd7d91d2b5e0ceb600f5c4438e574a0
Stephan Gabler <stephan.gabler@gmail.com>
2011-03-28 16:06:31 +0200

    first

---
+++ smt_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+this is a very simple python script to do some first experiments with
+sumatra
+"""
+
+import sys
+import os
+
+
+with open('test.data', 'w') as f:
+    f.write("bla bla\n")
+    
+
